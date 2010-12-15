18ffdb952a91d5e1d4821217f2c4bdb623fd38a4
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-12-15 10:35:56 -0200

    Ajuste contatos

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
     #se precisar os parametros são Email.deliver_padrao(corpo, "Assunto",email para&nbsp;&nbsp; #enviar(destino), email para resposta)
-    def enviar(corpo, assunto, email = "rmatuoka@agenciaeconomica.com.br",responder = "rmatuoka@agenciaeconomica.com.br")
+    def enviar(corpo, assunto, email = "contato@clinicaalmeria.com.br",responder = "contato@clinicaalmeria.com.br")
         recipients email
         from "rmatuoka@agenciaeconomica.com.br"
         subject assunto
