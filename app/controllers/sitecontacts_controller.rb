b07a964148186a51ac95b367ca03312092142941
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-12-15 10:34:48 -0200

    Ajuste contatos

--- app/controllers/sitecontacts_controller.rb
+++ app/controllers/sitecontacts_controller.rb
@@ -19,7 +19,7 @@ class SitecontactsController < ApplicationController
     if @Contato.save
       #ENVIA EMAIL
       @corpo = "Nome: #{@Contato.name} <br>E-mail: #{@Contato.email} <br>Telefone: #{@Contato.phone} <br>Empresa: #{@Contato.company} <br>Cidade: #{@Contato.city} <br>Estado: #{@Contato.state} <br>Mensagem: #{@Contato.message} <br>"
-      Notifier.deliver_enviar(@corpo.to_s,"Contato Via SITE", @Contato.email)
+      Notifier.deliver_enviar(@corpo.to_s,"Contato Via SITE", "contato@clinicaalmeria.com.br")
       
       redirect_to sitecontact_path(@Contato)
     else
